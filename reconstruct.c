d01ec1ecc558cb234327bbbda312541ff7e4c9e3
flynnbr11 <flynnbr@tcd.ie>
2015-11-09 21:14:57 +0000

    Doesn't work fully: returns a few rows at the top of the image. When run on multiple processors, if(rank==0) loop towards bottom not completing.

--- reconstruct.c
+++ reconstruct.c
@@ -8,9 +8,12 @@ int main(int argc, char *argv[])
 {
 
 MPI_Init(NULL, NULL);
+double begin_timer = MPI_Wtime();
+double end_timer, time_taken;
 int P, rank;
 MPI_Comm_size(MPI_COMM_WORLD, &P);
 MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+printf("There are %d processors, this is rank %d \n", P, rank);
 
 int M = 192;
 int N = 128;
@@ -18,10 +21,11 @@ float masterbuf[M][N];
 
 if(rank==0){
    pgmread("edge192x128.pgm", masterbuf, M, N);
+ //  printf("In first rank 0 if loop \n");
 }
 
 int i,j,t,T;
-T=50000;
+T=200;
 int Mp, Np;
 Mp=M/P;
 Np=N;
@@ -30,7 +34,6 @@ float buf[Mp][Np];
 float old[Mp+2][Np+2];
 float edge[Mp+2][Np+2];
 float new[Mp+2][Np+2];
-
 
 MPI_Scatter(masterbuf, product, MPI_INT, buf, product, MPI_INT, 0, MPI_COMM_WORLD);
 
@@ -72,14 +75,24 @@ for(t=0; t<T;t++){
       }
    }    
 
+//printf("Before mpi barrier, rank %d \n", rank);
+MPI_Barrier(MPI_COMM_WORLD);
+//printf("After mpi barrier, rank %d \n", rank);
+
+MPI_Gather(buf, product, MPI_INT, masterbuf, product, MPI_INT, 0, MPI_COMM_WORLD);
 MPI_Barrier(MPI_COMM_WORLD);
 
 if(rank==0){
-   MPI_Gather(buf, product, MPI_INT, masterbuf, product, MPI_INT, 0, MPI_COMM_WORLD);
-   pgmwrite("output.png", masterbuf,M, N);
+   pgmwrite("output_parallel.png", masterbuf,M, N);
+
+   printf("After printing image \n");
+   end_timer = MPI_Wtime();
+   time_taken = end_timer - begin_timer;
+   printf("Total time taken : %f \n", time_taken);
 }
 
 MPI_Finalize;
+printf("At end \n");
 
 return 0;
 }
